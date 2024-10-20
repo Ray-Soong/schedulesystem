e3e50a02139b0910dcd7fbd444c74ee58e47f897
root <root@lcm-test.novalocal>
2024-10-21 01:20:56 +0300

    Refine task len match total len

--- scheduler/src/components/TaskPool.vue
+++ scheduler/src/components/TaskPool.vue
@@ -1,48 +1,54 @@
 <template>
-    <div class="task-pool">
-      <h3>任务池</h3>
-      <div
-        v-for="task in tasks"
-        :key="task.id"
-        class="task-item"
-        draggable="true"
-        @dragstart="onDragStart(task)"
-      >
-        {{ task.name }} ({{ task.hours }}小时)
-      </div>
+  <div class="task-pool">
+    <h3>任务池</h3>
+    <div
+      v-for="task in tasks"
+      :key="task.id"
+      class="task-item"
+      :style="{ width: taskWidth(task.hours) + '%' }"
+      draggable="true"
+      @dragstart="onDragStart(task)"
+    >
+      {{ task.name }} ({{ task.hours }}小时)
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "TaskPool",
-    data() {
-      return {
-        tasks: [
-          { id: 1, name: "任务A", hours: 2 },
-          { id: 2, name: "任务B", hours: 4 },
-          { id: 3, name: "任务C", hours: 3 },
-        ],
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskPool",
+  data() {
+    return {
+      tasks: [
+        { id: 1, name: "任务A", hours: 2 },
+        { id: 2, name: "任务B", hours: 4 },
+        { id: 3, name: "任务C", hours: 3 },
+      ],
+    };
+  },
+  methods: {
+    onDragStart(task) {
+      event.dataTransfer.setData("task", JSON.stringify(task));
     },
-    methods: {
-      onDragStart(task) {
-        event.dataTransfer.setData("task", JSON.stringify(task));
-      },
+    taskWidth(hours) {
+      return (hours / 8) * 100;
     },
-  };
-  </script>
-  
-  <style>
-  .task-pool {
-    border: 1px solid #ccc;
-    padding: 10px;
-  }
-  .task-item {
-    margin: 10px;
-    padding: 5px;
-    background-color: #f0f0f0;
-    cursor: move;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+.task-pool {
+  border: 1px solid #ccc;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+}
+.task-item {
+  margin: 10px 0;
+  padding: 5px;
+  background-color: #f0f0f0;
+  cursor: move;
+  box-sizing: border-box;
+}
+</style>
--- scheduler/src/components/WorkstationPool.vue
+++ scheduler/src/components/WorkstationPool.vue
@@ -1,41 +1,40 @@
 <template>
-    <div class="workstation-pool">
-      <h3>工位池</h3>
-      <FullCalendar
-        :plugins="[timeGridPlugin, interactionPlugin]" 
-        initialView="timeGridWeek"
-        :slotMinTime="'08:00:00'"
-        :slotMaxTime="'18:00:00'"
-        @eventReceive="onTaskDrop"
-        ref="calendar"
-      />
-    </div>
-  </template>
-  
-  <script>
-  import FullCalendar from "@fullcalendar/vue3";
-  
-  export default {
-    name: "WorkstationPool",
-    components: { FullCalendar },
-    methods: {
-      onTaskDrop(info) {
-        const task = JSON.parse(info.draggedEl.dataset.task);
-        const calendarApi = this.$refs.calendar.getApi();
-        calendarApi.addEvent({
-          title: task.name,
-          start: info.date,
-          duration: { hours: task.hours },
-        });
-      },
+  <div class="workstation-pool">
+    <h3>工位池</h3>
+    <FullCalendar
+      :plugins="[timeGridPlugin, interactionPlugin]"
+      initialView="timeGridWeek" 
+      :slotMinTime="'08:00:00'"
+      :slotMaxTime="'18:00:00'"
+      @eventReceive="onTaskDrop"
+      ref="calendar"
+    />
+  </div>
+</template>
+
+<script>
+import FullCalendar from "@fullcalendar/vue3";
+
+export default {
+  name: "WorkstationPool",
+  components: { FullCalendar },
+  methods: {
+    onTaskDrop(info) {
+      const task = JSON.parse(info.draggedEl.dataset.task);
+      const calendarApi = this.$refs.calendar.getApi();
+      calendarApi.addEvent({
+        title: task.name,
+        start: info.date,
+        duration: { hours: task.hours },
+      });
     },
-  };
-  </script>
-  
-  <style>
-  .workstation-pool {
-    border: 1px solid #ccc;
-    padding: 10px;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+.workstation-pool {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
